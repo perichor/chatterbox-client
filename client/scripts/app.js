8ce927b478b1937a3c4165312fbffebe79c5c779
perichor <sandejeremy@gmail.com>
2017-01-03 16:45:22 -0800

    implement rooms functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,29 @@
 var app = {
   server: 'https://api.parse.com/1/classes/messages',
+  timeStamp: (new Date()).toISOString(),
   init: function() {
     //do stuff on load and consistently throughout
     // i.e. auto-update
+    $('.roomname').val('lobby');
+    $('.submit').on('click', function() {
+      // app.send(app.getInfo());
+      $('.textbox').val('');
+      var newRoom = $('.roomname').val();
+      var rooms = $('#roomSelect').children();
+      var found = false;
+      for (var i = 0; i < rooms.length; i++) {
+        if (rooms[i].innerHTML === newRoom) {
+          found = true;
+        }
+      }
+      if (!found) {
+        app.renderRoom(newRoom);
+      }
+      found = false;
+    });
+    $('.retrieve').on('click', function() {
+      app.fetch();
+    });
   },
 
   send: function(message) {
@@ -26,26 +47,37 @@ var app = {
   fetch: function() {
     var obj = {
       createdAt: {
-        $gte: '2017-01-01T00:00:00.000Z'
+        $gt: this.timeStamp
       },
       roomname: this.getInfo().roomname
     };
     var query = '?where=' + JSON.stringify(obj);
     this.server = 'https://api.parse.com/1/classes/messages' + query;
-    var result = $.ajax({
+    var response = $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: this.server,
       type: 'GET',
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+
+        var result = response.responseJSON.results;
+        if (result.length !== 0) {
+          app.timeStamp = result[0].createdAt;
+          for (var i = result.length - 1; i >= 0; i--) {
+            app.renderMessage(result[i]);
+          }
+
+          console.log(result);
+          console.log('chatterbox: Message grabbed');
+        }
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
+        console.error('chatterbox: Failed to grab message', data);
       }
     });
-    console.log(result);
+    
+    // console.log(result);
   },
 
   getInfo: function() {
@@ -54,17 +86,53 @@ var app = {
     info.text = $('.textbox').val();
     info.roomname = $('.roomname').val();
     return info;
+  },
+
+  createPost: function(object) {
+    //scrub here! DJ
+    var strip = function(html) {
+      var tmp = document.createElement('DIV');
+      console.log('after doc create');
+      tmp.innerHTML = html;
+      tmp = tmp.textContent || tmp.innerText || '';
+      return tmp.replace(/'/g, '\'');
+    };
+
+    object.username = strip(object.username);
+    object.text = strip(object.text);
+
+    // var userScrubRe = /^(?=[a-zA-Z0-9]*$)(?!.*[<>'"/;`%~@#$^*()_+=[\]{}|\\,.?: -])/;
+    // var textScrubRe = /^(?=[a-zA-Z0-9,.?:;]*$)(?!.*[<>'"/`%~@#$^*()_+=[\]{}|\\ -])/;
+    // var userClean = object.username.match(userScrubRe);
+    // var textClean = object.text.match(textScrubRe);
+    // if (!!userClean && !!textClean) {
+    let userDiv = '<div class="userdiv">' + object.username + ':</div>';
+    let textDiv = '<div class="textdiv">' + object.text + '</div>';
+    object.$node = '<div class="post ' + object.username + '">' + userDiv + textDiv + '</div>';
+    // }
+  },
+
+  renderMessage: function(message) {
+    app.createPost(message);
+    $('#chats').prepend(message.$node);
+  },
+
+  clearMessages: function() {
+    $('#chats').children().remove();
+  },
+
+  renderRoom: function(room) {
+    $('#roomSelect').append('<div>' + room + '</div>');
+  },
+
+  showRooms: function() {
+    $('#roomSelect').toggle();
   }
 };
+
+
 $('document').ready(function() {
   app.init();
-  $('.submit').on('click', function() {
-    app.send(app.getInfo());
-    $('.textbox').val('');
-  });
-  $('.retrieve').on('click', function() {
-    app.fetch();
-  });
 });
 
-  
+//on mouse move rekd them
